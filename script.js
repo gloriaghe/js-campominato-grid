abd5edc422d36fdf20bdf00ad7a64a2a1189b74a
Gloria <gloghe@yahoo.it>
2022-05-13 16:28:35 +0200

    numeri

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// chiedere a utente livello di difficoltà
-const livelloScelto = prompt("Scegli un livello da 1 a 3");
+
 
 // funzione numeri random
 let randommynum = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
@@ -17,7 +16,52 @@ function creaRandomNum(maxRange, min, max) {
     }
     return arreyInt;
 } 
+
 // creare blocchetti tramite js
+const createmyElement = (elemAggiuto, classAggiuta) => {
+    const node = document.createElement(elemAggiuto);
+    node.className = classAggiuta;
+    return node;
+}
+
+const inputUser = document.querySelector("input");
+const InizaGioco = document.querySelector("button");
+const GridPagina = document.getElementById("grid");
+
+
+InizaGioco.addEventListener("click",
+    function() {
+
+        inpuntUserParseInt = parseInt(inputUser.value)
+
+        if (1 === inpuntUserParseInt) {
+            for (let i = 0; i < 100; i++){
+              const crea100 =  createmyElement("div", "square1 square");
+              const numrandom100 = creaRandomNum(100, 1, 100);
+              crea100.append(numrandom100[i]);
+              grid.append(crea100);
+            }
+        } else if (inpuntUserParseInt == 2){
+            for (let i = 0; i < 81; i++){
+                const crea81 = createmyElement("div", "square2 square")
+                const numrandom81 = creaRandomNum(81, 1, 81);
+                crea81.append(numrandom81[i]);
+                grid.append(crea81)
+            }
+        } else if (inpuntUserParseInt == 3){
+            for (let i = 0; i < 49; i++){
+                const crea49 =  createmyElement("div", "square3 square")
+                const numrandom49 = creaRandomNum(49, 1, 49);
+                crea49.append(numrandom49[i]);
+                grid.append(crea49)
+ 
+            }
+        }
+
+
+    }
+
+)
 
 // aggiungere classi ai blocchetti
 
